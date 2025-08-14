8ef1a0834a427cd4781b77e25a00455ccb584332
Tasbeeha Khan <tasbeehakhan05@gmail.com>
2025-08-14 14:56:27 +0500

    revise 6 chapter

--- scipt.js
+++ scipt.js
@@ -1,2 +1,201 @@
 // Yahoo baba tutorial
 
+/*document.write("<b>Hi Tasbeeha<br></b>");
+document.write("<i> I am a passioanate web developer</i> ");*/
+
+
+/*var z = "hi <br>";
+var x = "hello";
+z = "hola <br>"
+x = "Adab"
+
+document.write(z );
+document.write(x)*/
+
+
+// snake case
+/*var my_name = "tasbeeha<br>";
+document.write(my_name);
+
+// camel case
+var yourCast = "Tasbeeha khan<br>";
+document.write(yourCast);
+
+
+// pascal case
+
+var YourAim = "being a developer<br>";
+document.write(YourAim);
+
+
+// numerical case
+
+var favnum9 = "9 is my lucky number";
+document.write(favnum9);
+
+
+var fav9num = "9 is my lucky number";
+document.write(fav9num);*/ 
+
+ 
+
+/*var x = "hello";____string
+var x = 34;_____numeric
+var x = true;____noolean
+var x = ["html","css","js"];____array
+var x ={first:"jane", last:"foe"};____object
+var x = null;_null
+var x;____undefined*/
+
+
+
+/*var x= "khan";
+document.write("<br>");
+document.write(x);
+document.write(typeof x);
+
+var a = 67 ;
+document.write("<br>");
+document.write(a);
+document.write(typeof a);
+
+
+var b ;
+document.write("<br>");
+document.write(b);
+document.write(typeof b);
+
+var c = true ;
+document.write("<br>");
+document.write(c);
+document.write(typeof c);
+
+
+var d = ["one","two","three"] ;
+document.write("<br>");
+document.write(d);
+document.write(typeof d);
+
+
+var e = {name:"tasbeeha",cast: "khan"} ;
+document.write("<br>");
+document.write(e);
+document.write(typeof e);*/
+
+
+
+var a = 2;
+var b = 6;
+var c = 2+6;
+document.write("2+6= ",c);
+
+
+var d = 56;
+var e = 30;
+var f = 56-30;
+document.write("<br>");
+document.write("56-30= ", f);
+
+var d = 2;
+var e = 6;
+var f = 2*6;
+document.write("<br>");
+document.write( "2*6= ",f);
+
+var g= 2;
+var h = 6;
+var i = 2/6;
+document.write("<br>");
+document.write( "2/6= ",i);
+
+
+var j= 2;
+var k = 6;
+var l = 2%6;
+document.write("<br>");
+document.write( "2%6= ",l);
+
+var m= 2;
+var n = 6;
+var o = 2**6;
+document.write("<br>");
+document.write("2**6= ",o);
+
+var p= 4;
+var q = 3;
+var r = p++ + ++p + p++ + p++ + q++ + ++q + q++;
+document.write("<br>");
+document.write(r);
+
+
+var p= 4;
+var q = 3;
+var u = ++p + ++p + ++p + p++ + ++p + p++ + p++ + q++ + q++ + ++q + ++q +q++
+document.write("<br>");
+document.write(u);
+
+
+
+var a = 5;
+var b = 3;
+var c = a++ + --a + ++a + b-- + ++b + --b;
+document.write("<br>");
+document.write(c);
+
+
+var x = 7;
+var y = 2;
+var z = --x + x++ + ++x + y++ + --y + y--;
+document.write("<br>");
+document.write(z);
+
+
+var i = 4;
+var n = 6;
+var k = i-- + i++ + --i + ++i + n-- + --n + ++n;
+document.write("<br>");
+document.write(k);
+
+
+var m = 5;
+var n = 4;
+var o = ++m + m-- + --m + m++ + ++n + n++ + --n + n--;
+document.write("<br>");
+document.write(o);
+
+
+var a = 4;
+var b = 5;
+var result = a++ + ++a + b-- - --b + a++ - b++ + ++b;
+document.write("<br>");
+document.write(result);
+
+var x = 7;
+var y = 3;
+var results = ++x - x++ + y++ + ++y - --x + y-- - ++y;
+document.write("<br>");
+document.write(results);
+
+
+var p = 2;
+var q = 6;
+var result = p++ + p + ++p - q-- + ++q + q++ - --p + --q;
+document.write("<br>");
+document.write(result);
+
+
+var m = 5;
+var n = 2;
+var result = ++m + m++ + --m - n++ + ++n + --n + n++ - ++m + m--;
+document.write("<br>");
+document.write(result);
+
+
+
+var s = 3;
+var t = 4;
+var result = s++ + ++s + s + t++ - ++t + t-- - s-- + ++t + --s;
+document.write("<br>");
+document.write(result);
+
+
