497172c878949c667a334cfa248f155c0aea2de4
Tasbeeha Khan <tasbeehakhan05@gmail.com>
2025-08-15 01:30:12 +0500

    Updated revise  15 chapters

--- scipt.js
+++ scipt.js
@@ -84,7 +84,7 @@ document.write(typeof e);*/
 
 
 
-var a = 2;
+/*var a = 2;
 var b = 6;
 var c = 2+6;
 document.write("2+6= ",c);
@@ -196,6 +196,290 @@ var s = 3;
 var t = 4;
 var result = s++ + ++s + s + t++ - ++t + t-- - s-- + ++t + --s;
 document.write("<br>");
-document.write(result);
+document.write(result);*/
+
+
+/*var a = 10;
+var b = 10;
+document.write(a==b)
+document.write("<br>");
+
+var a = 10;
+var b = "10";
+document.write("<br>");
+document.write(a===b)
+
+var a = 10;
+var b = 7;
+document.write("<br>");
+document.write(a<b)
+
+var a = 4;
+var b = 6;
+document.write("<br>");
+document.write(a>b)
+
+var a = 4;
+var b = 6;
+document.write("<br>");
+document.write(a!=b)
+
+
+var a = 4;
+var b = 4;
+document.write("<br>");
+document.write(b!==a)
+
+
+
+var a = 4;
+var b = 4;
+document.write("<br>");
+document.write(a<=b)
+
+
+var a = 4;
+var b = 1;
+document.write("<br>");
+document.write(a<=b)
+
+
+var a = 4;
+var b = 1;
+document.write("<br>");
+document.write(a>=b)
+
+var a = 4;
+var b = 9;
+document.write("<br>");
+document.write(a>=b)
+
+
+var a;
+var b = null;
+document.write("<br>");
+document.write(a>=b)
+
+var a = null;
+var b ;
+document.write("<br>");
+document.write(a==b)
+
+
+var c;
+var d = null;
+document.write("<br>");
+document.write(c===d)*/
+
+
+/*var a = 200;
+var b = 200;
+
+if(a == b){
+document.write("A is Greater")
+}*/
+
+
+/*var age = 19 ;
+
+if (age >= 18 && age <=33){
+    document.write("you are 19");
+}*/
+
+
+/*var age = 19 ;
+
+if (age >= 18 || age <=3){
+    document.write("you are 19");
+}*/
+
+
+/*var a = 700;
+var b = 200;
+
+if(a !== b){
+document.write("A is Greater")
+}*/
+
+
+/*var a = 700;
+var b = 200;
+
+if(!a < b){
+document.write("xyz")
+}*/
+
+
+/*var x = 5;
+if(x>30){
+    document.write("5 is small");
+
+}else{
+    document.write("30 is greater")
+}
+
+document.write("<br>");
+
+var x = 5;
+if(x>2){
+    document.write("5 is small");
+
+}else{
+    document.write("30 is greater")
+}
+
+
+
+document.write("<br>");
+
+var x = 50;
+if(x==50){
+    document.write("50 is same");
+
+}else{
+    document.write("50 is not same")
+}
+
+
+document.write("<br>");
+
+var x = 50;
+if(x === "50"){
+    document.write("50 is same");
+
+}else{
+    document.write("50 is not same")
+}
+
+
+document.write("<br>");
+
+var name = "tasbeeha";
+var gender = "female";
+
+if(gender === "male"){
+    document.write("Hello Mr." + name);
+
+}else{
+    document.write("Hello Miss"+ name)
+}*/
+
+
+/*var percentage = +prompt("enter a number and check you performance");
+
+if(percentage>= 80 && percentage <= 100){
+    document.write("you are in merit");
+}else if (percentage >=60 && percentage <80 ){
+    document.write("you are in 1st division");
+}else if (percentage >=45 && percentage <60 ){
+    document.write("you are in 2st division");
+}else if(percentage>= 33 && percentage <45 ){
+    document.write("you are in 3rd division");
+}else if(percentage <33 ){
+    document.write("you are in fail");
+}else{
+    document.write("please enter a valid number");
+}*/
+
+
+
+/*var a = 5;
+var b = 8;
+if(a > b){
+    alert(a);
+}else{
+    alert(b);
+}
+
+
+var a = 5;
+var b = 8;
+if(a > b){
+    alert(a+b);
+}else{
+    alert("value of 8:" + a+b);
+}*/
+
+
+
+/*for(var i=1; i <=10; i++){
+    document.write("hi tasbeeha <br>")
+}*/
+
+
+/*for(var i=1; i <=10; i++){
+    if(i ==3){
+       document.write(" Hey : "+ i + "<br>") ;
+       continue;
+    }
+    document.write(" Number : "+ i + "<br>")
+}*/
+
+
+
+/*for(var i=1; i <=10; i++){
+    if(i ==3){
+       document.write(" Hey : "+ i + "<br>") ;
+       break;
+    }
+    document.write(" Number : "+ i + "<br>")
+}*/
+
+
+/*for(var a=1; a<=100; a++){
+    if(a% 2 != 0){
+        document.write(a+ "<br>");
+    }
+}
+
+
+for(var a=1; a<=100; a++){
+    if(a% 2 == 0){
+        document.write(a+ "<br>");
+    }
+}*/
+
+/*for(var a =1; a<= 100; a = a+10){
+    for(var b = a; b < a+10 ; b++){
+        document.write(b + " ");
+    }
+    document.write("<br>")
+}
+
+
+Outer Loop (a) → 1
+   Inner Loop (b) → 1  2  3  4  5  6  7  8  9  10
+   [line break]
+
+Outer Loop (a) → 11
+   Inner Loop (b) → 11  12  13  14  15  16  17  18  19  20
+   [line break]
+
+Outer Loop (a) → 21
+   Inner Loop (b) → 21  22  23  24  25  26  27  28  29  30
+   [line break]
+
+Outer Loop (a) → 31
+   Inner Loop (b) → 31  32  33  34  35  36  37  38  39  40
+   [line break]
+
+...
+(Ye pattern 100 tak chalta hai)*/
+
+
+for(var a =1 ; a<=5; a++){
+    for (var b=1; b<=a; b++){
+        document.write(a + " ");
+    }
+    document.write("<br>")
+}
+
+
+for(var a =1 ; a<=5; a++){
+    for (var b=1; b<=a; b++){
+        document.write(b + " ");
+    }
+    document.write("<br>")
+}
 
 
